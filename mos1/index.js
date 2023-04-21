bdbdd598fdd287486052ec9f34f40e9da926cfdf
VIMLESH KUMAR <vimlesh1975@hotmail.com>
2023-04-21 20:36:31 +0530

    working

--- mos1/index.js
+++ mos1/index.js
@@ -1,62 +1,57 @@
-const { MosDevice, MosConnection, ConnectionConfig, MosModel } = require('@mos-connection/connector')
-const mosClient = new MosConnection({
-  mosID: 'my-mos-client',
-  acceptsConnections: true,
-  profiles: {
-    '0': true,
-    '1': true,
-    '2': true,
-    '4': true
-  },
-  openRelay: true,
-  debug: true
+import { MosConnection, ConnectionConfig } from '@mos-connection/connector'
+
+const mos = new MosConnection(
+	new ConnectionConfig({
+		mosID: 'sofie.server.tv.automation',
+		acceptsConnections: true,
+		openRelay: true,
+		profiles: {
+			'0': true,
+			'1': true,
+		},
+		// To start the server on custom ports:
+		// ports: {
+		// 	lower: 11540,
+		// 	upper: 11541,
+		// 	query: 11542,
+		// },
+		debug: false,
+	})
+)
+mos.on('error', (err) => {
+	console.log('MosConnection emitted error', err, err.stack)
 })
+mos.onConnection((mosDevice) => {
+	console.log('A new Mosdevice connected')
 
-mosClient.onConnection((mosdevice) => {
-  console.log('MOS client connected')
-  mosdevice.onConnectionChange(() => {
-    console.log('MOS device connected',mosdevice.ID)
-    mosdevice.onRequestMachineInfo(message => {
-      console.log(`Received MOS message: ${message.toXMLString()}`)
-    })
-    mosdevice.socket.on('error', (err) => {
-      console.error('Socket error:', err);
-    });
-    
-    
-  })
+	const mosTypes = mosDevice.mosTypes // Could also be retrieved with getMosTypes(strict)
+
+	mosDevice.onRequestMachineInfo(async () => {
+		return {
+			manufacturer: mosTypes.mosString128.create('mommy'),
+			model: mosTypes.mosString128.create('model!'),
+			hwRev: mosTypes.mosString128.create('0.1'),
+			swRev: mosTypes.mosString128.create('1.0'),
+			DOM: mosTypes.mosString128.create('1989-07-01'),
+			SN: mosTypes.mosString128.create('1234'),
+			ID: mosTypes.mosString128.create('MY ID'),
+			time: mosTypes.mosTime.create(Date.now()),
+			// opTime?: mosTypes.mosTime.create(),
+			mosRev: mosTypes.mosString128.create('A'),
+
+			supportedProfiles: {
+				deviceType: 'MOS',
+				profile0: true,
+			},
+		}
+	})
+	mosDevice.onRequestAllRunningOrders(async () => {
+		return []
+	})
 })
-
-mosClient.init()
-mosClient.connect({ primary: { id: 'mosID', host: '127.0.0.1' } })
-
-
-const mosClient2 = new MosConnection({
-  mosID: 'my-mos-client-2',
-  acceptsConnections: true,
-  profiles: {
-    '0': true,
-    '1': true,
-    '2': true,
-    '4': true
-  },
-  openRelay: true,
-  debug: true,
-  ports: { lower: 11000, query: 12000, upper: 13000 }
-})
+mos.init()
+	.then(() => console.log('initialized'))
+	.catch(console.log)
 
 
-mosClient2.onConnection((mosdevice) => {
-  console.log('MOS client 2 connected')
-  mosdevice.onConnectionChange(() => {
-    console.log('MOS device 2 connected')
-    mosdevice.onRequestMachineInfo(message => {
-      console.log(`Received MOS message: ${message.toXMLString()}`)
-    })
-  })
-})
-
-
-
-mosClient2.init()
-mosClient2.connect({ primary: { id: 'my-mos-client' , host: '127.0.0.1' }})
+  
--- mostest/main.js
+++ mostest/main.js
@@ -1,36 +1,153 @@
-const { MosConnection } = require('@mos-connection/connector')
+import {
+	MosConnection,
+	ConnectionConfig,
+} from '@mos-connection/connector'
 
+const mos = new MosConnection(
+	new ConnectionConfig({
+		mosID: 'sofie.client.tv.automation',
+		acceptsConnections: true,
+		profiles: {
+			'0': true,
+			'1': true,
+		},
+    ports: {
+      lower: 11540,
+      upper: 11541,
+      query: 11542,
+    },
+		openRelay: true,
+		debug: false,
+	})
+)
+// mos.on('rawMessage', (_source, _type, _message) => {
+// 	console.log('rawMessage', _source, _type, _message)
+// })
+mos.on('error', (err) => {
+	console.log('MosConnection emitted error', err, err.stack)
+})
+mos.onConnection((mosDevice) => {
+	console.log('new mosDevice: ', mosDevice.idPrimary, mosDevice.idSecondary)
+	// console.log(dev)
 
-const mosClient2 = new MosConnection({
-  mosID: 'my-mos-client-2',
-  acceptsConnections: true,
-  profiles: {
-    '0': true,
-    '1': true,
-    '2': true,
-    '4': true
-  },
-  openRelay: true,
-  debug: true,
-  ports: { lower: 11000, query: 12000, upper: 13000 }
+	const mosTypes = mosDevice.mosTypes // Could also be retrieved with getMosTypes(strict)
+
+	let hasSentInit = false
+
+	mosDevice.onConnectionChange((status) => {
+		console.log('connection status: ', status)
+		if (!hasSentInit) {
+			sendInit().catch(console.error)
+			hasSentInit = true
+		}
+	})
+
+	const sendInit = async () => {
+		const machineInfo = await mosDevice.requestMachineInfo()
+		console.log('Machineinfo', machineInfo)
+
+		const ros = await mosDevice.sendRequestAllRunningOrders()
+		console.log('allRunningOrders', ros)
+
+		// trigger a re-send of those running orders:
+		// return dev.getRunningOrder(mosTypes.mosString128.create('696297DF-1568-4B36-B43B3B79514B40D4'))
+
+		const roLists = await Promise.all(
+			ros.map(async (ro) => {
+				return mosDevice.sendRequestRunningOrder(ro.ID)
+			})
+		)
+		console.log('roLists', roLists)
+	}
+	mosDevice.onRequestMachineInfo(async () => {
+		return {
+			manufacturer: mosTypes.mosString128.create('mommy'),
+			model: mosTypes.mosString128.create('model!'),
+			hwRev: mosTypes.mosString128.create('0.1'),
+			swRev: mosTypes.mosString128.create('1.0'),
+			DOM: mosTypes.mosString128.create('1989-07-01'),
+			SN: mosTypes.mosString128.create('1234'),
+			ID: mosTypes.mosString128.create('MY ID'),
+			time: mosTypes.mosTime.create(Date.now()),
+			// opTime?: mosTypes.mosTime.create(),
+			mosRev: mosTypes.mosString128.create('A'),
+
+			supportedProfiles: {
+				deviceType: 'MOS',
+				profile0: true,
+			},
+		}
+	})
+	mosDevice.onReadyToAir(async (Action)=> {
+		console.log('ready to air', Action)
+		return {
+			ID: Action.ID,
+			Status: mosTypes.mosString128.create('OK'),
+			Stories: [],
+		}
+	})
+
+	mosDevice.onCreateRunningOrder(async (ro) => {
+		console.log('create running order', ro)
+		return {
+			ID: ro.ID,
+			Status: mosTypes.mosString128.create('OK'),
+			Stories: [],
+		}
+	})
+
+	mosDevice.onDeleteRunningOrder(async (RunningOrderID) => {
+		console.log('delete running order', RunningOrderID)
+		return {
+			ID: RunningOrderID,
+			Status: mosTypes.mosString128.create('OK'),
+			Stories: [],
+		}
+	})
+	mosDevice.onROInsertStories(async (Action, Stories) => {
+		console.log('insert stories', Action, Stories)
+		return {
+			ID: Action.StoryID,
+			Status: mosTypes.mosString128.create('OK'),
+			Stories: [],
+		}
+	})
+
+	mosDevice.onROMoveStories(async (Action, Stories)=> {
+		console.log('move stories', Action, Stories)
+		return {
+			ID: Action.StoryID,
+			Status: mosTypes.mosString128.create('OK'),
+			Stories: [],
+		}
+	})
+
+	mosDevice.onRODeleteStories(async (Action, Stories)=> {
+		console.log('delete stories', Action, Stories)
+		return {
+			ID: Action.RunningOrderID,
+			Status: mosTypes.mosString128.create('OK'),
+			Stories: [],
+		}
+	})
 })
 
-
-mosClient2.onConnection((mosdevice) => {
-  console.log('MOS client 2 connected')
-  mosdevice.onConnectionChange(() => {
-    console.log('MOS device 2 connected', mosdevice)
-    mosdevice.onRequestMachineInfo(message => {
-      console.log(`Received MOS message: ${message.toXMLString()}`)
-    })
-
-
-  })
-})
-
-
-
-mosClient2.init()
-// mosClient2.connect({ primary: { id: 'mosID', host: '127.0.0.1' ,  ports: { lower: 11000, query: 12000, upper: 13000 }} })
-mosClient2.connect({ primary: { id: 'mosID', host: '127.0.0.1' }})
-
+mos.init()
+	.then(async (_listening) => {
+		return mos.connect({
+			primary: {
+				id: 'sofie.server.tv.automation',
+				host: '127.0.0.1',
+				timeout: 100000,
+				// To connect to a server on custom ports:
+				// ports: {
+				// 	lower: 11540,
+				// 	upper: 11541,
+				// 	query: 11542,
+				// },
+			},
+		})
+	})
+	.catch((e) => {
+		console.log(e)
+	})
