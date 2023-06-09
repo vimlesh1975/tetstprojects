9ef26758ff779e547fd57ba7fa50842e733009b6
VIMLESH KUMAR <vimlesh1975@hotmail.com>
2023-06-09 11:35:26 +0530

    removed unused

--- differentFont/main.js
+++ differentFont/main.js
@@ -1,7 +1,23 @@
-const aa= document.getElementsByClassName('fname')[0].getElementsByTagName('tspan')[0]
-console.log(aa.textContent)
 
-const firstName=aa.textContent.split(' ')[0];
-const lastName=aa.textContent.split(' ')[1];
-// console.log(aa.innerText.split(' ')[0])
-aa.innerHTML=(`<tspan style="font-family: Impact;">${firstName}</tspan> <tspan style="font-family: cursive;">${lastName}</tspan>`)
+const applyDifferentfont=()=>{
+    const aa= document.getElementsByClassName('fname')[0].getElementsByTagName('tspan')[0]
+    const firstName=aa.textContent.split(' ')[0];
+    const lastName=aa.textContent.split(' ')[1];
+    aa.innerHTML=(`<tspan style="font-family: Impact;">${firstName}</tspan> <tspan style="font-family: cursive;">${lastName}</tspan>`)
+}
+
+// applyDifferentfont();
+
+var old_dataInsert = dataInsert;
+dataInsert = function () {
+    old_dataInsert.apply(this, arguments);
+    for (var idCaspar in dataCaspar) {
+        if (idCaspar.includes('f0')) {
+            const aa= document.getElementsByClassName('fname')[0].getElementsByTagName('tspan')[0]
+            const firstName=aa.textContent.split(' ')[0];
+            const lastName=aa.textContent.split(' ')[1];
+            aa.innerHTML=(`<tspan style="font-family: Impact;">${firstName}</tspan> <tspan style="font-family: cursive;">${lastName}</tspan>`)
+        }
+    }
+};
+
