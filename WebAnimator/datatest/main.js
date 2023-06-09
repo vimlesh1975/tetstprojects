1b5a57e99e926e0ccc0deaf5844e13fe5237bcac
VIMLESH KUMAR <vimlesh1975@hotmail.com>
2023-06-09 11:48:39 +0530

    Added datatest

---
+++ WebAnimator/datatest/main.js
@@ -0,0 +1,30 @@
+console.log('hi')
+const aa = [
+    'एसआरए प्रकल्पातल्या कथित घोटाळ्याप्रकरणी',
+    'प्रकाश मेहतांच्या राजीनाम्याची मागणी एसआरए प्रकल्पातल्या कथित घोटाळ्याप्रकरणी करत',
+    'विरोधी एसआरए प्रकल्पातल्या कथित घोटाळ्याप्रकरणी पक्षांकडून ',
+    'विधिमंडळाच्या दोन्ही सभागृहांत गदारोळ',
+    'एअर मार्शल हेमंत भागवत यांची भारतीय वायू सेनेच्या प्रशासकीय विभाग प्रमुखपदी नियुक्ती',
+]
+var i = 0;
+var element;
+setTimeout(() => {
+    // console.log(studio);
+    // console.log(sheet);
+    // console.log(sheet.sequence);
+    // console.log(sheet.sequence.position);
+    sheet.sequence.position = 0;
+    element = canvas.getObjects()[0];
+    updatestring('ccgf0', aa[i].toString())
+}, 100);
+
+setInterval(() => {
+    sheet.sequence.position = 0;
+    sheet.sequence.play({ range: [0, 2] });
+
+    updatestring('ccgf0', aa[i].toString())
+    i += 1;
+    if (i > aa.length - 1) {
+        i = 0;
+    }
+}, 3000);
